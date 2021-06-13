26d9869b55615148723c8a0ae2d2cf946343100b
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2021-06-14 01:38:42 +0200

    k

---
+++ Project-Kaas/Assets/Collection/Network/Scripts/TestConnection.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Collection.Network.Scripts
+{
+    public class TestConnection : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
