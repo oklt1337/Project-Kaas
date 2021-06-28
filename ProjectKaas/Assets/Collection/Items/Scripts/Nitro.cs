75930c2f00f1f3637bc975afe2cac509b0c0111e
LPAde <andre-peters01@hotmail.de>
2021-06-28 23:27:59 +0200

    nitro base
    Added the groundwork for the nitro item

---
+++ ProjectKaas/Assets/Collection/Items/Scripts/Nitro.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Collection.Items.Scripts
+{
+    public class Nitro : ItemBehaviour
+    {
+        public override void OnUse()
+        {
+            //TODO: Get owner as car and give it speed up.
+            base.OnUse();
+        }
+    }
+}
