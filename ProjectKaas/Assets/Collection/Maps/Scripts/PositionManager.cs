388442907b66002a23411297923f9cacd93474fa
LPAde <andre-peters01@hotmail.de>
2021-07-07 17:24:37 +0200

    updates to position manager
    all convex

--- ProjectKaas/Assets/Collection/Maps/Scripts/PositionManager.cs
+++ ProjectKaas/Assets/Collection/Maps/Scripts/PositionManager.cs
@@ -11,8 +11,12 @@ namespace Collection.Maps.Scripts
         [SerializeField] private List<PlayerHandler> allPlayers;
         [SerializeField] private List<PlayerHandler> allPlayersPositions;
 
+        [SerializeField] private byte lapCount;
+        
         [field: SerializeField] public GameObject[] Zones { get; }
 
+        public byte LapCount => lapCount;
+        
         public List<PlayerHandler> AllPlayers => allPlayers;
         
         private void Awake()
@@ -21,6 +25,86 @@ namespace Collection.Maps.Scripts
                 Destroy(this);
             
             PositionManagerInstance = this;
+        }
+
+        private void Update()
+        {
+            DeterminePositions();
+        }
+
+        /// <summary>
+        /// Updates the List of all player positions.
+        /// </summary>
+        private void DeterminePositions()
+        {
+            // No need when only 1 player.
+            if(allPlayers.Count == 1)
+                return;
+            
+            var players = SeparatedByLaps(allPlayersPositions);
+            allPlayersPositions = SeparatedByZones(players);
+        }
+        
+        /// <summary>
+        /// Separates the players by their laps.
+        /// </summary>
+        /// <param name="players"> The player list you want to have sorted. </param>
+        /// <returns> a list of players that is separated by their laps</returns>
+        private PlayerHandler[,] SeparatedByLaps(List<PlayerHandler> players)
+        {
+            var separatedPlayers = new PlayerHandler[lapCount,players.Count];
+
+            for (var i = 0; i < players.Count; i++)
+            {
+                for (var j = 0; j < players.Count; j++)
+                {
+                    // Sorts Players by their laps.
+                    if (separatedPlayers[players[i].Car.LapCount, j] == null) 
+                        continue;
+                    
+                    separatedPlayers[players[i].Car.LapCount, j] = players[i];
+                    break;
+                }
+            }
+            
+            return separatedPlayers;
+        }
+
+        private List<PlayerHandler> SeparatedByZones(PlayerHandler[,] players)
+        {
+            /*
+             * car8, car1, car2, car5 
+             * car7,car3
+             * car4,car6
+             *
+             * Go Through ONE lap count -> compare Zones -> sort by Zones
+             */
+            List<PlayerHandler> sortedPlayers = null;
+            
+            // Repeated for every lap backwards.
+            for (int i = LapCount; i > 0; i--)
+            {
+                // Skips process when no player is at that lap. 
+                if(players[i,0] == null)
+                    continue;
+
+                // Shortens process when only one player is at that lap.
+                if (players[i, 1] == null)
+                {
+                    sortedPlayers.Add(players[i,0]);
+                    continue;
+                }
+                
+                
+            }
+            
+            
+            return sortedPlayers;
+        }
+
+        private List<PlayerHandler> FinalList(PlayerHandler[,] players)
+        {
+            return null;
         }
 
         /// <summary>
@@ -38,6 +122,12 @@ namespace Collection.Maps.Scripts
                 {
                     nextPlayer = allPlayersPositions[i - 1];
                 }
+            }
+
+            // Targets the last player when no one is in front of current player.
+            if (nextPlayer == null)
+            {
+                nextPlayer = allPlayersPositions[allPlayersPositions.Count - 1];
             }
             
             return nextPlayer;
