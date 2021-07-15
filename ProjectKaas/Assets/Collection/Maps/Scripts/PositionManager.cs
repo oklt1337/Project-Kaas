3b24503abdd9e2656aeb2add5aa9ffd838c55693
LPAde <andre-peters01@hotmail.de>
2021-07-15 23:49:58 +0200

    asdsafd

--- ProjectKaas/Assets/Collection/Maps/Scripts/PositionManager.cs
+++ ProjectKaas/Assets/Collection/Maps/Scripts/PositionManager.cs
@@ -73,7 +73,9 @@ namespace Collection.Maps.Scripts
             
             allPlayersPositions = null;
             var players = SeparatedByLaps(allPlayersPositions);
-            allPlayersPositions = SeparatedByZones(players);
+            
+            if(players != null)
+                allPlayersPositions = SeparatedByZones(players);
         }
         
         /// <summary>
@@ -83,6 +85,9 @@ namespace Collection.Maps.Scripts
         /// <returns> a list of players that is separated by their laps</returns>
         private PlayerHandler[,] SeparatedByLaps(List<PlayerHandler> players)
         {
+            if (players == null)
+                return null;
+            
             var separatedPlayers = new PlayerHandler[lapCount, players.Count];
 
             for (var i = 0; i < players.Count; i++)
